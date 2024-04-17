85cc37aaf3e5d0b843a972b9fe04a00c3dfb49a8
benkabundi01 <benkabundi01@gmail.com>
2024-04-17 00:43:46 -0400

    First Commit: Adding client and server side code

--- userFullStack_Server/src/main/java/com/fullstack/userfullstack_server/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fullstack.userfullstack_server.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException (Long user_id) {
-        super("Could Not Find User With ID: " + user_id);
-    }
-}

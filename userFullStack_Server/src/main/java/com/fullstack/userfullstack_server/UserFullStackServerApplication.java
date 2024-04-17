39b495e935ac2bc35f859a4cf49f3623277c2c31
benkabundi01 <benkabundi01@gmail.com>
2024-04-17 00:32:40 -0400

    First Commit: Already added the client and server side codes

---
+++ userFullStack_Server/src/main/java/com/fullstack/userfullstack_server/UserFullStackServerApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.userfullstack_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFullStackServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserFullStackServerApplication.class, args);
+    }
+
+}
---
+++ userFullStack_Server/src/test/java/com/fullstack/userfullstack_server/UserFullStackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.userfullstack_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFullStackServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
